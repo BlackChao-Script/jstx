ef9fd9b5b1c392f8c873262d40d4c665c13fb532
xuzhichao <c199188177@163.com>
2022-12-02 15:51:02 +0800

    注册页面完成

--- src/pages/minikol/register/index.vue
+++ src/pages/minikol/register/index.vue
@@ -11,6 +11,61 @@
     <Logo></Logo>
     <view class="register-box">
       <view class="box-title">注册</view>
+      <view style="margin-top: 30rpx">
+        <u--form :model="model1" :rules="rules" ref="form1">
+          <u-form-item prop="userInfo.name" borderBottom ref="item1">
+            <u--input
+              v-model="model1.userInfo.name"
+              border="none"
+              placeholder="请取个名字"
+            ></u--input>
+          </u-form-item>
+          <u-form-item prop="userInfo.emil" borderBottom ref="item1">
+            <u--input
+              v-model="model1.userInfo.emil"
+              border="none"
+              placeholder="请输入邮箱"
+            ></u--input>
+          </u-form-item>
+          <u-form-item prop="userInfo.pwd" borderBottom ref="item1">
+            <!-- #ifdef H5 -->
+            <u--input
+              v-model="model1.userInfo.pwd"
+              :password="showPwt"
+              border="none"
+              placeholder="这里输入密码"
+              suffixIconStyle="font-size: 40rpx;"
+            >
+              <template slot="suffix">
+                <u-icon
+                  size="40"
+                  :name="showPwt == true ? 'eye-fill' : 'eye-off'"
+                  @click="() => (showPwt = !showPwt)"
+                ></u-icon>
+              </template>
+            </u--input>
+            <!--  #endif -->
+            <!--  #ifdef  MP-WEIXIN -->
+            <u-input
+              v-model="model1.userInfo.pwd"
+              :password="showPwt"
+              border="none"
+              placeholder="这里输入密码"
+              suffixIconStyle="font-size: 40rpx;"
+            >
+              <template slot="suffix">
+                <u-icon
+                  size="40"
+                  :name="showPwt == true ? 'eye-fill' : 'eye-off'"
+                  @click="() => (showPwt = !showPwt)"
+                ></u-icon>
+              </template>
+            </u-input>
+            <!--  #endif -->
+          </u-form-item>
+        </u--form>
+        <view class="box-button">注册</view>
+      </view>
     </view>
   </view>
 </template>
@@ -20,18 +75,40 @@ import Nav from '@common/Nav.vue'
 import Logo from '@common/Logo.vue'
 
 export default {
-  data() {
-    return {
-      leftImg: require('@/assets/img/向左.png'),
-      errImg: require('@/assets/img/错.png')
-    }
-  },
   components: {
     Nav,
     Logo
   },
-  mounted() {},
-
+  data() {
+    return {
+      leftImg: require('@/assets/img/向左.png'),
+      errImg: require('@/assets/img/错.png'),
+      model1: {
+        userInfo: {
+          name: '',
+          emil: '',
+          pwd: ''
+        }
+      },
+      rules: {
+        'userInfo.name': {
+          type: 'string',
+          required: true,
+          message: '请填写姓名',
+          trigger: ['blur', 'change']
+        },
+        'userInfo.emil': {
+          type: 'string',
+          required: true,
+          message: '请填写邮箱',
+          trigger: ['blur', 'change']
+        }
+      },
+      suffixIcon: 'eye-fill',
+      suffixIcon: 'eye-fill',
+      showPwt: true
+    }
+  },
   methods: {}
 }
 </script>
@@ -43,6 +120,16 @@ export default {
     .box-title {
       font-weight: 600;
       font-size: 50rpx;
+    }
+    .box-button {
+      margin: 150rpx auto;
+      width: 500rpx;
+      height: 100rpx;
+      border-radius: 50rpx;
+      text-align: center;
+      line-height: 100rpx;
+      color: #fff;
+      background-color: #d4d4d6;
     }
   }
 }
