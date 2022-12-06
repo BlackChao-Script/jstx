4e8d6995df1b0a1998f206beea824b95798267c9
xuzhichao <c199188177@163.com>
2022-12-06 17:04:24 +0800

    聊天制作中

---
+++ src/pages/minikol/chitchat/index.vue
@@ -0,0 +1,127 @@
+<template>
+  <view class="chitchat">
+    <view style="border-bottom: 1rpx solid #ededed">
+      <Nav>
+        <template v-slot:left>
+          <view @click="toBack"
+            ><u--image :src="leftImg" width="50rpx" height="50rpx" mode="widthFix"></u--image
+          ></view>
+        </template>
+        <template v-slot:mid>
+          <view style="font-size: 35rpx">开心就好</view>
+        </template>
+        <template v-slot:right>
+          <view></view>
+        </template>
+      </Nav>
+      <view class="chitchat-list"></view>
+    </view>
+    <scroll-view class="chat" :scroll-y="true" :scroll-with-animation="true">
+      <view class="chat-main">
+        <view class="chat-ls">
+          <view class="ls-time">12:32</view>
+          <view class="ls-msg msg-left" v-for="v in 3">
+            <view class="msg">
+              <image :src="userImg" class="msg-img"></image>
+            </view>
+            <view class="msg-content">
+              <view class="content-text"
+                >你好我的哦SD撒旦萨达萨达萨达是阿斯顿撒旦撒大苏打撒啊撒撒旦撒发达发达</view
+              >
+            </view>
+          </view>
+        </view>
+        <view class="chat-ls">
+          <view class="ls-time">12:32</view>
+          <view class="ls-msg msg-right" v-for="v in 2">
+            <view class="msg">
+              <image :src="userImg" class="msg-img"></image>
+            </view>
+            <view class="msg-content">
+              <view class="content-text">你好我的哦SD撒旦萨达</view>
+            </view>
+          </view>
+        </view>
+      </view>
+    </scroll-view>
+  </view>
+</template>
+
+<script>
+import Nav from '@/common/nav.vue'
+
+export default {
+  components: { Nav },
+  data() {
+    return {
+      leftImg: require('@/assets/img/向左.png'),
+      userImg: 'https://cdn.uviewui.com/uview/album/5.jpg'
+    }
+  },
+
+  methods: {
+    toBack() {
+      this.toBackPage()
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.chat {
+  height: 100%;
+  .chat-main {
+    padding-left: 30rpx;
+    padding-right: 30rpx;
+    padding-bottom: 120rpx;
+    display: flex;
+    flex-direction: column;
+    .chat-ls {
+      .ls-time {
+        font-size: 25rpx;
+        color: rgba(39, 40, 50, 0.3);
+        padding: 20rpx;
+        text-align: center;
+      }
+      .ls-msg {
+        display: flex;
+        padding: 20rpx 0;
+        .msg {
+          width: 90rpx;
+          .msg-img {
+            width: 90rpx;
+            height: 90rpx;
+            border-radius: 10rpx;
+          }
+        }
+        .msg-content {
+          max-width: 480rpx;
+          background-color: aqua;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          padding: 10rpx;
+          margin: 0 20rpx;
+          .content-text {
+            font-size: 35rpx;
+          }
+        }
+      }
+      .msg-left {
+        flex-direction: row;
+        .msg-content {
+          border-radius: 0 10rpx 10rpx;
+          background-color: #f4f4f4;
+        }
+      }
+      .msg-right {
+        flex-direction: row-reverse;
+        .msg-content {
+          border-radius: 10rpx 0 10rpx 10rpx;
+          background-color: #ffe431;
+        }
+      }
+    }
+  }
+}
+</style>
