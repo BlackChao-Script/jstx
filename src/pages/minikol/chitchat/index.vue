4ac32d5ad634d1c40a9472e518f1677d2f73d172
xuzhichao <c199188177@163.com>
2022-12-07 17:01:15 +0800

    修改聊天对话列表

--- src/pages/minikol/chitchat/index.vue
+++ src/pages/minikol/chitchat/index.vue
@@ -15,27 +15,17 @@
     </view>
     <scroll-view class="chat" :scroll-y="true" :scroll-with-animation="true">
       <view class="chat-main">
-        <view class="chat-ls">
-          <view class="ls-time">12:32</view>
-          <view class="ls-msg msg-left" v-for="v in 5">
+        <view class="chat-ls" v-for="value in mes" :key="value.tip">
+          <view class="ls-time">{{ value.time }}</view>
+          <view class="ls-msg msg-left" :class="[value.id === 'a' ? 'msg-left' : 'msg-right']">
             <view class="msg">
-              <image :src="userImg" class="msg-img"></image>
+              <image :src="value.imgUrl" class="msg-img"></image>
             </view>
             <view class="msg-content">
-              <view class="content-text"
-                >你好我的哦SD撒旦萨达萨达萨达是阿斯顿撒旦撒大苏打撒啊撒撒旦撒发达发达</view
-              >
-            </view>
-          </view>
-        </view>
-        <view class="chat-ls">
-          <view class="ls-time">12:32</view>
-          <view class="ls-msg msg-right" v-for="v in 2">
-            <view class="msg">
-              <image :src="userImg" class="msg-img"></image>
-            </view>
-            <view class="msg-content">
-              <view class="content-text">你好我的哦SD撒旦萨达</view>
+              <view class="content-text" v-if="value.type === 0">{{ value.message }}</view
+              ><view class="content-img" v-else-if="value.type === 1">
+                <image :src="value.imgUrl" class="img" mode="widthFix"></image>
+              </view>
             </view>
           </view>
         </view>
@@ -60,6 +50,7 @@
 
 <script>
 import Nav from '@/common/nav.vue'
+import mes from '@/utils/data.js'
 
 export default {
   components: { Nav },
@@ -70,13 +61,29 @@ export default {
       boxLeftImg: require('@/assets/img/语音.png'),
       boxRightImg: require('@/assets/img/表情.png'),
       boxRightImga: require('@/assets/img/更多.png'),
-      value: ''
+      value: '',
+      mes
     }
   },
-
+  onLoad() {
+    this.getData(this.mes)
+  },
   methods: {
     toBack() {
       this.toBackPage()
+    },
+    getData(n) {
+      for (let i of n) {
+        let now = new Date(i.time),
+          m = now.getMonth() + 1,
+          d = now.getDate()
+        i.time =
+          (m < 10 ? '0' + m : m) +
+          '月' +
+          (d < 10 ? '0' + d : d) +
+          '日' +
+          now.toTimeString().substr(0, 5)
+      }
     }
   }
 }
@@ -85,11 +92,11 @@ export default {
 <style lang="scss">
 .chitchat {
   .chat {
+    padding-bottom: 200rpx;
     height: 100%;
     .chat-main {
       padding-left: 30rpx;
       padding-right: 30rpx;
-      padding-bottom: 120rpx;
       display: flex;
       flex-direction: column;
       .chat-ls {
@@ -112,29 +119,39 @@ export default {
           }
           .msg-content {
             max-width: 480rpx;
-            background-color: aqua;
             display: flex;
             justify-content: center;
             align-items: center;
-            padding: 10rpx;
             margin: 0 20rpx;
             .content-text {
               font-size: 35rpx;
+              padding: 20rpx;
+            }
+            .content-img {
+              width: 400rpx;
+              .img {
+                width: 100%;
+                border-radius: 10rpx;
+              }
             }
           }
         }
         .msg-left {
           flex-direction: row;
           .msg-content {
-            border-radius: 0 10rpx 10rpx;
-            background-color: #f4f4f4;
+            .content-text {
+              border-radius: 0 10rpx 10rpx;
+              background-color: #f4f4f4;
+            }
           }
         }
         .msg-right {
           flex-direction: row-reverse;
           .msg-content {
-            border-radius: 10rpx 0 10rpx 10rpx;
-            background-color: #ffe431;
+            .content-text {
+              border-radius: 10rpx 0 10rpx 10rpx;
+              background-color: #ffe431;
+            }
           }
         }
       }
@@ -143,7 +160,7 @@ export default {
 }
 .chitchat-nav {
   position: fixed;
-  border-top: 1rpx solid #ececec;
+  border-top: 1rpx solid #ededed;
   background-color: #fff;
   width: 100%;
   height: 120rpx;
---
+++ src/utils/data.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    id: 'a',
+    imgUrl: 'https://cdn.uviewui.com/uview/album/5.jpg',
+    message: 'sadsdasdfcvjcvhdijouijnc',
+    type: 0, // 内容类型 (0文字、1图片、2音频链接)
+    time: new Date() - 1000,
+    tip: 0
+  },
+  {
+    id: 'a',
+    imgUrl: 'https://cdn.uviewui.com/uview/album/5.jpg',
+    message: 'xczxczxsadqwdoko',
+    type: 0, // 内容类型 (0文字、1图片、2音频链接)
+    time: new Date() - 1000 * 16,
+    tip: 1
+  },
+  {
+    id: 'b',
+    imgUrl: 'https://cdn.uviewui.com/uview/album/5.jpg',
+    message: '你撒滴哦偶才收到i第四Ohio分好低哦分和第三方好的',
+    type: 0, // 内容类型 (0文字、1图片、2音频链接)
+    time: new Date() - 1000 * 32,
+    tip: 2
+  },
+  {
+    id: 'b',
+    imgUrl: 'https://cdn.uviewui.com/uview/album/5.jpg',
+    message: '参加女厕仙女iOS大家佛i的时间覅伺机待发',
+    type: 0, // 内容类型 (0文字、1图片、2音频链接)
+    time: new Date() - 1000 * 36,
+    tip: 3
+  },
+  {
+    id: 'a',
+    imgUrl: 'https://cdn.uviewui.com/uview/album/5.jpg',
+    message: 'https://cdn.uviewui.com/uview/album/5.jpg',
+    type: 1, // 内容类型 (0文字、1图片、2音频链接)
+    time: new Date() - 1000 * 64,
+    tip: 4
+  },
+  {
+    id: 'a',
+    imgUrl: 'https://cdn.uviewui.com/uview/album/5.jpg',
+    message: 'https://cdn.uviewui.com/uview/album/5.jpg',
+    type: 1, // 内容类型 (0文字、1图片、2音频链接)
+    time: new Date() - 1000 * 121,
+    tip: 5
+  }
+]
